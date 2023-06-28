4c619689d03c504a30b79becdb5c12e2b24e15ab
Dakotady <Dakotadyche@gmail.com>
2023-06-28 10:28:17 -0600

    working update.

--- src/Applicaiton/LoginScreen.java
+++ src/Applicaiton/LoginScreen.java
@@ -1,4 +1,11 @@
 package Applicaiton;
 
+import javafx.event.ActionEvent;
+
 public class LoginScreen {
+
+
+    public void exitApplicaitonOnClicked(ActionEvent actionEvent) {
+        System.exit(1);
+    }
 }
--- src/Applicaiton/Main.java
+++ src/Applicaiton/Main.java
@@ -2,11 +2,15 @@ package Applicaiton;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Main extends Application implements Initializable {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -19,5 +23,9 @@ public class Main extends Application {
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
     }
 }
