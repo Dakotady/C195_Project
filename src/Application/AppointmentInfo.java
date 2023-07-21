ba22473bed05c176d16499a5a5fe2f6ef40241aa
Dakotady <Dakotadyche@gmail.com>
2023-07-20 19:53:28 -0600

    working update.

---
+++ src/Application/AppointmentInfo.java
@@ -0,0 +1,25 @@
+package Application;
+
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+
+public class AppointmentInfo {
+    public TextField appointmentIDText;
+    public ComboBox contact;
+    public TextField title;
+    public TextField description;
+    public TextField location;
+    public ComboBox startTime;
+    public DatePicker startDate;
+    public ComboBox endTime;
+    public DatePicker endDate;
+    public ComboBox customerID;
+    public ComboBox userID;
+    public Text customerName;
+    public Text userName;
+    public Button cancel;
+    public Button confirm;
+}
